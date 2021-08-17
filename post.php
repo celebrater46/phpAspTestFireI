515c6b06dbf5b255e0edcd4eaf060c051d8d8ca7
hideru <jota.16.stigma.world@gmail.com>
2021-08-18 04:07:55 +0900

    Tested SSL_POST again but it is still 200

--- post.php
+++ post.php
@@ -5,33 +5,33 @@
 
 ini_set('display_errors', "On");
 
-//$url = 'https://localhost:44395/api/Members/';
-$url = 'http://localhost:63279/api/Members/';
+$url = 'https://localhost:44395/api/Members/';
+//$url = 'http://localhost:63279/api/Members/';
 
 $data = array(
-    'Name' => 'Sally',
-    'Age' => '34',
+    'Name' => 'Miyon',
+    'Age' => '28',
     'HireDate' => '2018-06-28T00:00:00',
 );
 
 $data = json_encode($data); // JSONに変換
 
 $context = array(
-    'http' => array(
-//    'ssl' => array(
+//    'http' => array(
+    'ssl' => array(
         'method'  => 'POST',
 //        'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
-        'header'  => implode("\r\n", array('Content-Type: application/json',)),
-//        'header'  => 'Content-Type: application/json',
+//        'header'  => implode("\r\n", array('Content-Type: application/json',)),
+        'header'  => 'Content-Type: application/json',
 //        'content' => http_build_query($data),
         'content' => $data,
-//        'verify_peer' => false,
-//        'verify_peer_name' => false,
+        'verify_peer' => false,
+        'verify_peer_name' => false,
     )
 );
 
 $html = file_get_contents($url, false, stream_context_create($context));
 
-var_dump($http_response_header);
+//var_dump($http_response_header);
 //var_dump($context);
-//echo $html;
+echo $html;
