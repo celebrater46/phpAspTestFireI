d8f41ad0f38e057ec4b5115766fa20a238a08bdc
hideru <jota.16.stigma.world@gmail.com>
2021-08-17 15:30:24 +0900

    fixed bugs

--- index.php
+++ index.php
@@ -1,47 +1,5 @@
 <?php
 
-// Refer: https://qiita.com/fantm21/items/603cbabf2e78cb08133e JSONの取得
-// Refer: https://qiita.com/izanari/items/f4f96e11a2b01af72846 SSLエラーについて
-// API: DB_Ope_API
-// https://qiita.com/okdyy75/items/d21eb95f01b28f945cc6 PHP POST送信について
-
-function getApi() {
-    $url = "https://localhost:44395/api/members";
-
-    $options['ssl']['verify_peer']=false;
-    $options['ssl']['verify_peer_name']=false;
-
-    //$json = file_get_contents($url);
-    $json = file_get_contents($url, false, stream_context_create($options));
-    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-    $arr = json_decode($json,true);
-
-    var_dump($arr);
-}
-
-function postApi() {
-    $url = 'https://localhost:44395/api/members';
-
-    $data = array(
-        'name' => 'Aizawa',
-        'age' => '58',
-        'fireDate' => '2021/08/18',
-    );
-
-    $context = array(
-        'http' => array(
-            'method'  => 'POST',
-            'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
-            'content' => http_build_query($data)
-        )
-    );
-
-    $html = file_get_contents($url, false, stream_context_create($context));
-
-    var_dump($http_response_header);
-//    echo $html;
-}
-
-//getApi();
-postApi();
+echo "Hello World." . PHP_EOL;
+echo "Access get.php or post.php..." . PHP_EOL;
 
